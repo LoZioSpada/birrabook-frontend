fb01547c2807a42ed925d5621c598bce081b9166
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-01-27 19:05:44 +0100

    birrabook-frontend
    - Iniziato ad impostare il frontend;
    - Implementato funzione per il tema chiaro/scuro in base al tema del dispositivo che si usa;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import './App.css';
+import { useState } from "react"
+import "./App.scss"
+import { useEffect } from "react"
+import { Container } from "react-bootstrap"
 
 function App() {
-  return
+    // funzione che cambia il tema chiaro/scuro in base a quello impostato dal dispositivo
+    const [isDarkMode, setIsDarkMode] = useState(false)
 
+    useEffect(() => {
+        const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark")
+        setIsDarkMode(darkModeQuery.matches)
+
+        const handleThemeChange = (event) => {
+            setIsDarkMode(event.matches)
+        }
+
+        darkModeQuery.addEventListener("change", handleThemeChange)
+        return () => {
+            darkModeQuery.removeEventListener("change", handleThemeChange)
+        }
+    }, [])
+    return (
+        <div className={isDarkMode ? "dark-mode" : "light-mode"}>
+            <MyNavbar />
+            <Container></Container>
+        </div>
+    )
 }
 
-export default App;
+export default App
